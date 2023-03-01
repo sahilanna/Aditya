0e569719c227af8e098d7da77124cfbf8603c941
sahilanna <126063782+sahilanna@users.noreply.github.com>
2023-03-01 14:04:58 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import Demo from "./Demo";
+import Product from './components/Product';
+import ProductList from './containers/ProductList';
+
+
+function App() {
+  return (
+    <div className="App">
+     
+    <Demo />
+    </div>
+  );
+}
+
+export default App;
